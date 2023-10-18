c080257150043d98d1e7758ca1d6e03dc5d2f725
kecolima <kecolima@hotmail.com>
2023-10-18 11:56:59 -0300

    endpoint findAll para user

--- src/main/java/br/com/alexlima/api/services/UserService.java
+++ src/main/java/br/com/alexlima/api/services/UserService.java
@@ -2,8 +2,10 @@ package br.com.alexlima.api.services;
 
 import br.com.alexlima.api.domain.User;
 
-public interface UserService {
+import java.util.List;
 
+public interface UserService {
         User findById(Integer id);
+        List<User> findAll();
 
 }
