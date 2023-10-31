4227f5efd1977a97aba5cfc3c0ba3b92776708e3
kecolima <kecolima@hotmail.com>
2023-10-31 15:59:51 -0300

    teste para endpoint update da classe user resource implementado

--- src/test/java/br/com/alexlima/api/resources/UserResourceTest.java
+++ src/test/java/br/com/alexlima/api/resources/UserResourceTest.java
@@ -110,7 +110,8 @@ class UserResourceTest {
         assertNotNull(response);
         assertNotNull(response.getBody());
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(ResponseEntity.class, response.getBody().getClass());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
 
         assertEquals(ID, response.getBody().getId());
         assertEquals(NAME, response.getBody().getName());
